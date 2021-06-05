220f6f3e227c624bf9e3af8aecda68eeebbaaa73
1417MHz <ysw99@kakao.com>
2021-06-05 17:50:48 +0900

    colorchoose

--- popup.py
+++ popup.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from tkinter import *
+from tkinter import ttk
+from tkinter import colorchooser
+
 
 # 사용자 편의 기능 2. 폰트 설정 팝업 표시
 class StylePopup():
@@ -10,7 +13,7 @@ class StylePopup():
         self.frame = Toplevel()
         
         TITLE = "스타일 설정, P.Y Editor 1.0"
-        self.frame.geometry('400x300')
+        self.frame.geometry('300x200')
         
         self.frame.title(TITLE)
         
@@ -21,27 +24,67 @@ class StylePopup():
         lbFont = Label(frame, text='글꼴', width=10)
         lbFontStyle = Label(frame, text='글꼴 스타일')
         lbFontSize = Label(frame, text='크기')
+        lbFgColor = Label(frame, text='글자 색상')
+        inFgColorSelected = Entry(frame, width=13)
+        lbBgColor = Label(frame, text='배경 색상')
+        inBgColorSelected = Entry(frame, width=13)
         
         lbFont.grid(row=0, column=0)
         lbFontStyle.grid(row=1, column=0)
         lbFontSize.grid(row=2, column=0)
+        lbFgColor.grid(row=3, column=0)
+        inFgColorSelected.grid(row=3, column=1)
+        lbBgColor.grid(row=4, column=0)
+        inBgColorSelected.grid(row=4, column=1)
 
-        inFont = Entry(frame, width=20)
-        inFontStyle = Entry(frame)
-        inFontSize = Entry(frame)
+        # 글꼴 선택 Combobox
+        cbFont = ttk.Combobox(frame, values=[
+                                        "Gothic", 
+                                        "D2Coding",
+                                        "Courier"], width=10)
+        cbFont.current(0)
+
+        # 글꼴 스타일 선택 Combobox
+        cbFontStyle = ttk.Combobox(frame, values=[
+                                        "italic",
+                                        "bold",
+                                        "underline"
+                                        # plain is NULL 
+                                        ], width=10)
+        cbFontStyle.current()
+
+        # 글자 크기 선택 Combobox
+        cbFontSize = ttk.Combobox(frame, values=[
+                                        9,
+                                        10,
+                                        11,
+                                        12,
+                                        15,
+                                        16,
+                                        20], width=10)
+        cbFontSize.current(4)
         
-        inFont.grid(row=0, column=1)
-        inFontStyle.grid(row=1, column=1)
-        inFontSize.grid(row=2, column=1)
+        # 색상표 호출
+        fgColorBtn = Button(frame, text = "색상 선택", command = self.chooseFgColor)
+        bgColorBtn = Button(frame, text = "색상 선택", command = self.chooseBgColor)
 
-        self.inFont = inFont
-        self.inFontStyle = inFontStyle
-        self.inFontSize = inFontSize
+        # 위치 지정
+        cbFont.grid(row=0, column=1)
+        cbFontStyle.grid(row=1, column=1)
+        cbFontSize.grid(row=2, column=1)
+        fgColorBtn.grid(row=3, column=2)
+        bgColorBtn.grid(row=4, column=2)
+
+        self.cbFont = cbFont
+        self.cbFontStyle = cbFontStyle
+        self.cbFontSize = cbFontSize
+        self.inFgColorSelected = inFgColorSelected
+        self.inBgColorSelected = inBgColorSelected
 
         trueBtn = Button(frame, text='확인', command=self.onApply)
         falseBtn = Button(frame, text='취소', command=self.onCancel)
-        trueBtn.grid(row=3, column=0)
-        falseBtn.grid(row=3, column=1)
+        trueBtn.grid(row=5, column=0)
+        falseBtn.grid(row=5, column=1)
         
 
     def onCancel(self):
@@ -50,10 +93,28 @@ class StylePopup():
     def onApply(self):
         print("Destroy", self)
         options = dict({
-            "font": self.inFont.get().strip(),
-            "fontStyle": self.inFontStyle.get().strip(),
-            "fontSize": self.inFontSize.get().strip()
+            "font": self.cbFont.get().strip(),
+            "fontStyle": self.cbFontStyle.get().strip(),
+            "fontSize": self.cbFontSize.get().strip(),
+            "fgColor": self.inFgColorSelected.get().strip,
+            "bgColor": self.inBgColorSelected.get().strip
         })
         
         self.parent.on_child_popup_closed(self, options)
         self.frame.destroy()
+
+    def chooseFgColor(self):
+        # variable to store hexadecimal code of color
+        color_code = colorchooser.askcolor(title ="색상 선택")
+        self.inFgColorSelected.delete(0, 'end')
+        self.inFgColorSelected.insert(0, color_code[1])
+        print(color_code[1])
+        self.frame.lift()
+
+    def chooseBgColor(self):
+        # variable to store hexadecimal code of color
+        color_code = colorchooser.askcolor(title ="색상 선택")
+        self.inBgColorSelected.delete(0, 'end')
+        self.inBgColorSelected.insert(0, color_code[1])
+        print(color_code[1])
+        self.frame.lift()
