69b1a6a5e4734944e729eb25dfd78fa59abddd18
1417MHz <ysw99@kakao.com>
2021-06-05 16:44:49 +0900

    commit before merge

--- textEditor.py
+++ textEditor.py
@@ -26,6 +26,7 @@ class TextEditor():
         self.root.protocol("WM_DELETE_WINDOW", self.file_quit)
         self.make_menu()
         self.bind_events()
+        self.display_font_popup()
 
 
     def make_menu(self):
@@ -190,7 +191,4 @@ class TextEditor():
         print(popup)
 
     def setStyles(self, font='Gothic', font_size=15, color='black', bg_color='white'):
-        
-        
-        
-    
+        pass
